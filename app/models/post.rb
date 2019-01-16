5fa17fe9f518d31b36c14a9b715aa622d8109f6d
Grace Shih <vocalise@gmail.com>
2019-01-16 07:55:30 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,22 @@
 class TitleValidator < ActiveModel::EachValidator
+
     def validate_each(record, attribute, value)
         valid_titles = ["Won't Believe", "Secret", "Guess"]
-        if valid_titles.include?value or !(/Top \d+/.match(value)).nil?
+        if !(/Top \d+/.match(value)).nil?
           return true
-        else
-            record.errors[attribute] << "is not valid"
-          return false
         end
+        valid_titles.each do |valid_title|
+          if !value.nil? && (value.include? valid_title)
+             return true
+          end
+        end
+        record.errors[attribute] << "is not valid"        
+        return false
     end
 end
 
 class Post < ActiveRecord::Base
-    validates :title, presence: true, title: true
+    validates :title, title: true
     validates :summary, presence: true, length: {maximum: 250}
     validates :content, presence: true, length: {minimum: 250}
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
