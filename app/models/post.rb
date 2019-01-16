2c1da25c46ba3cddd9717cae746c7a16338b4e58
Grace Shih <vocalise@gmail.com>
2019-01-16 07:51:27 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class TitleValidator < ActiveModel::EachValidator
           return true
         end
         valid_titles.each do |valid_title|
-          if value.include? valid_title
+          if value.present && value.include? valid_title
              return true
           end
         end
