0aa761041f65b2a44b2b02c6565865da7e36566e
Grace S <vocalise@gmail.com>
2019-01-15 22:10:50 -0800

    Add validation statements to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
-class Post < ActiveRecord::Base
+class MyValidator < ActiveModel::Validator
+    def validate(title)
+        valid_titles = ["Won't Believe",  "Secret", "Guess"]
+        if valid_titles.include?title || title.match("Top /\d/+")
+            return true
+        end
+        return false
+    end
 end
+
+class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates :title, presence: true
+    validates :summary, presence: true, length: {maximum: 250}
+    validates :content, presence: true, length: {minimum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates_with MyValidator
+end  
+
+  
